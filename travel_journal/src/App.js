639d08c18a52fd8297938a3e1138adb470543caa
dennisloliver <doliver9110@gmail.com>
2021-11-02 11:47:04 -0400

    Added new Form file

---
+++ travel_journal/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {useState} from 'react'
+import axios from 'axios';
+// import NewForm from './components/NewForm'
+
+const App = () => {
+
+
+useEffect(()=>{
+  axios
+
+})
+
+return (
+<main>
+<h1>Travel Journal</h1>
+// <NewForm/>
+
+</main>
+)
+
+
+
+}
+
+
+export default App;
