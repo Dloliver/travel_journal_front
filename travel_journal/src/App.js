ae7580df35247cafed138dee6e58524b59a66fb0
dennisloliver <doliver9110@gmail.com>
2021-11-02 18:04:48 -0400

    added form

--- travel_journal/src/App.js
+++ travel_journal/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import NewForm from './components/NewForm'
 
 const App = () => {
   const [logs, setLogs] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:3000/travel-logs')
-    .then(res => {
+    axios
+    .get('http://localhost:3000/travel-logs')
+    .then((res) => {
       setLogs(res.data);
     });
   }, []);
@@ -15,7 +17,7 @@ const App = () => {
   return (
     <main>
       <h1>Travel Journal</h1>
-
+      <NewForm setLogs={setLogs}/>
     </main>
   );
 };
